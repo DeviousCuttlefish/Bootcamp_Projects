41f18b567ce56c673e77d607fedef1541591d600
DeviousCuttlefish <57159977+DeviousCuttlefish@users.noreply.github.com>
2020-03-06 14:26:07 -0700

    Python Django Upload

---
+++ bookstore-django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Hardings/Documents/GitHub/DeviousCuttlefish.github.io/bookstore-django/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
